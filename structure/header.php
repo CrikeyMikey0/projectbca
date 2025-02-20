cdfd17a5e47267de951573bdf7a614e6d9d003a6
CrikeyMikey0 <132225172+CrikeyMikey0@users.noreply.github.com>
2025-02-21 02:46:55 +0530

    fixed dual home screen glitch

--- structure/header.php
+++ structure/header.php
@@ -18,7 +18,77 @@
         </div>
     </div>
 
-    <!--Header-->
+<header id="main-navigation">
+  <div id="navigation" data-spy="affix" data-offset-top="20">
+    <div class="container">
+      <div class="row">
+      <div class="col-md-12">
+        <nav class="navbar navbar-default">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#fixed-collapse-navbar" aria-expanded="false"> 
+            <span class="icon-bar top-bar"></span> <span class="icon-bar middle-bar"></span> <span class="icon-bar bottom-bar"></span> 
+            </button>
+            <a class="navbar-brand logo-center" href="index.php"><img src="images/logo.png" alt="logo" class="img-responsive"></a>
+         </div>
+        
+            <div id="fixed-collapse-navbar" class="navbar-collapse collapse">
+              
+              <div class="navbar-left-1">
+              <ul class="nav navbar-nav  navbar-left">
+              <li><a href="index.php">Home</a></li>
+               <!-- <li class="dropdown active">
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Home</a>
+                   <ul class="dropdown-menu">
+                    <li><a href="index.php">Home V1</a></li>
+                    <li><a href="index2.php">Home V2</a></li>
+                  </ul>
+                </li> -->
+                <li><a href="food.php">Our Food</a></li>
+                <li class="dropdown">
+                  <a data-toggle="dropdown" href="#" class="dropdown-toggle">pages</a>
+                  <ul class="dropdown-menu">
+                    <li><a href="about.php">About Us</a></li>
+                    
+                    <li><a href="blog-detail.php">Blog Detail</a></li>
+                    <li><a href="faq.php">FAQ</a></li>
+                  </ul>
+                 </li>
+                  <li class="dropdown">
+                  <a data-toggle="dropdown" href="#" class="dropdown-toggle">gallery</a>
+                  <ul class="dropdown-menu">
+                    <li><a href="gallery.php">Gallery Three</a></li>
+                    <li><a href="gallery4c.php">Gallery Four column</a></li>
+                    <li><a href="gallery2c.php">Gallery Two column</a></li>
+                  </ul>
+                </li>
+                </ul> 
+                </div>
+                <div class="navbar-right-1">
+                <ul class="nav navbar-nav  navbar-right"> 
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="blog.php">blog</a></li>
+                <li><a href="order.php">Order Now</a></li>
+                <li class="dropdown">
+                  <a data-toggle="dropdown" href="#" class="dropdown-toggle">Location</a>
+                  <ul class="dropdown-menu">
+                    <li><a href="location.php">Location V1</a></li>
+                    <li><a href="location2.php">Location V2</a></li>
+                  </ul>
+                </li>
+              </ul>
+              </div>
+            </div>
+         </nav>
+         </div>
+       </div>
+     </div>
+  </div>
+</header>
+
+
+
+
+    <!--Header optional header
     <header id="main-navigation">
         <div id="navigation" data-spy="affix" data-offset-top="20">
             <div class="container">
@@ -54,4 +124,4 @@
             </div>
         </div>
     </header>
-
+Header-->
